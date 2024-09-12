f1ab538e482e0084eed024abcd3f8a2912bbb389
Willams Silva <willamssilvainfo@gmail.com>
2024-09-12 19:38:31 -0300

    Fundamentos 004

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-let meta = {
-    value: 'Ler um livro por mês',
-    checked: true
+const start = () => {
+    let count = 1;
+
+    while (count <= 10) {
+        console.log(count);
+        count = count + 1;
+    }
 }
 
-let metas = [
-    meta, {
-        value: 'Caminhar 20 minutos todos os dias',
-        checked: false
-    }
-]
-
-console.log(metas[0].value);
+start();
