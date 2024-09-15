18ca1c023086e0ce921f248b514d48bf15210c14
Willams Silva <willamssilvainfo@gmail.com>
2024-09-14 23:09:29 -0300

    Cadastrar meta

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const { select } = require('@inquirer/prompts');
+const { select, input } = require('@inquirer/prompts');
+
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log('A meta não pode ser vazia.')
+        return
+    }
+
+    metas.push(
+        { value: meta, checked: false }
+    )
+}
 
 const start = async () => {
 
@@ -9,11 +29,11 @@ const start = async () => {
             choices: [
                 {
                     name: "Cadastrar meta",
-                    value: "Cadastrar"
+                    value: "cadastrar"
                 },
                 {
                     name: "Listar metas",
-                    value: "Listar"
+                    value: "listar"
                 },
                 {
                     name: "Sair",
@@ -24,7 +44,8 @@ const start = async () => {
 
         switch (opcao) {
             case "cadastrar":
-                console.log("Vamos cadastrar");
+                await cadastrarMeta();
+                console.log(metas)
                 break;
             case "listar":
                 console.log("Vamos listar");
