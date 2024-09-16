4d9396cebef54f73fa926320602bc16dd7a06bfd
Willams Silva <willamssilvainfo@gmail.com>
2024-09-16 16:26:15 -0300

    Adiciona metas abertas

--- index.js
+++ index.js
@@ -58,9 +58,25 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: "Metas Realizadas" + realizadas.length,
         choices: [...realizadas]
     })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true;
+    });
+
+    if (abertas.length == 0) {
+        console.log("Não existem metas abertas! :)");
+        return;
+    }
+
+    await select ({
+        message: "Metas Abertas" + abertas.length,
+        choices: [...abertas]
+    });
 }
 
 const start = async () => {
@@ -83,6 +99,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -99,6 +119,9 @@ const start = async () => {
                 break;
             case "realizadas":
                 await metasRealizadas();
+                break;
+            case "abertas":
+                await metasAbertas();
                 break;
             case "sair":
                 console.log("Até a próxima");
