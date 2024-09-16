43354783f6475e18f7aad36f9dc1989253e2d59d
Willams Silva <willamssilvainfo@gmail.com>
2024-09-16 15:14:28 -0300

    Metas realizadas

--- index.js
+++ index.js
@@ -47,6 +47,22 @@ const listarMetas = async () => {
     console.log('Meta(s) marcadas como concluída(s)');
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked;
+    });
+
+    if (realizadas.length == 0) {
+        console.log("Não existem metas realizadas! :(");
+        return;
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+}
+
 const start = async () => {
 
     while (true) {
@@ -63,8 +79,12 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
-                    value: "Sair"
+                    value: "sair"
                 }
             ]
         })
@@ -77,7 +97,10 @@ const start = async () => {
             case "listar":
                 await listarMetas();
                 break;
-            case "Sair":
+            case "realizadas":
+                await metasRealizadas();
+                break;
+            case "sair":
                 console.log("Até a próxima");
                 return
         }
